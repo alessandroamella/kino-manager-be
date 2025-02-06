d29d35d264090530b7f7683e1abbd842247f8ee5
Alessandro Amella <info@bitrey.it>
2025-02-06 01:56:24 +0100

    prevent trustProxy

--- src/main.ts
+++ src/main.ts
@@ -86,6 +86,9 @@ async function bootstrap() {
     rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes
       max: 1000, // limit each IP to 1000 requests per windowMs
+      validate: {
+        trustProxy: false,
+      },
     }),
   );
 
