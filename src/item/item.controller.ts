b831ac20be86b8df595f49a0580049d50b1620e5
Alessandro Amella <info@bitrey.it>
2025-03-18 15:38:37 +0100

    menu no auth

--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -1,20 +1,18 @@
-import { UseGuards, Controller, Get, Post, Body } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import {
-  ApiTags,
   ApiBearerAuth,
-  ApiOperation,
   ApiOkResponse,
+  ApiOperation,
+  ApiTags,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 import { AdminGuard } from 'auth/admin.guard';
 import { JwtAuthGuard } from 'auth/jwt-auth.guard';
-import { ItemService } from './item.service';
 import { CreateItemDto } from './dto/create-item.dto';
 import { GetItemDto } from './dto/get-item.dto';
+import { ItemService } from './item.service';
 
 @ApiTags('item')
-@ApiBearerAuth()
-@UseGuards(JwtAuthGuard, AdminGuard)
 @Controller('item')
 export class ItemController {
   constructor(private readonly itemService: ItemService) {}
@@ -22,12 +20,13 @@ export class ItemController {
   @Get()
   @ApiOperation({ summary: 'Get all items with categories' })
   @ApiOkResponse({ description: 'All items', type: [GetItemDto] })
-  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
-  async geItemsWithCategories(): Promise<GetItemDto[]> {
+  async getItemsWithCategories(): Promise<GetItemDto[]> {
     return this.itemService.findAllWithCategories();
   }
 
   @Post()
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, AdminGuard)
   @ApiOperation({ summary: 'Create an item' })
   @ApiOkResponse({ description: 'Created item', type: GetItemDto })
   @ApiUnauthorizedResponse({ description: 'Unauthorized' })
